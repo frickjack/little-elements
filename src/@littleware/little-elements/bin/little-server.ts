6669c6cb6588aba41105763b32fd7654d9753e29
Reuben Pasquini <reuben@frickjack.com>
2018-06-14 00:01:59 -0500

    fix(little-server): accept command line args

--- src/@littleware/little-elements/bin/little-server.ts
+++ src/@littleware/little-elements/bin/little-server.ts
@@ -1,9 +1,22 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
 
-app.use( '/modules/', express.static( './node_modules/' ) );
-app.use( '/modules/@littleware/little-elements/lib/', express.static( './lib/' ));
-
+app.use('/modules/', express.static('./node_modules/'));
+{
+    const args = process.argv.splice(2);
+    if (args.length > 0 && args[0].match(/^-*h[elp]/i)) {
+        console.log( `
+Use: npx little-server urlPath1 folderPath1 urlPath2 folderPath2 ...
+        `);
+        process.exit(0);
+    }
+    for(let i=0; i+1 < args.length; i+=2) {
+        let urlPath = args[i];
+        let folderPath = args[i+1];
+        console.log(`serving ${urlPath} from ${folderPath}`);
+        app.use(urlPath, folderPath);
+    }
+}
 
 app.listen( 3000, function() {
     console.log( 'Server listening at http://localhost:3000/' );
