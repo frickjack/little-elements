0e4e3cb41cf6f124946e2b02370c7689e8f13339
Reuben Pasquini <reuben@frickjack.com>
2018-06-17 21:46:58 -0500

    fix(gulpfile): nunjucks build

--- gulpHelper.js
+++ gulpHelper.js
@@ -40,13 +40,12 @@ module.exports.defineTasks = function(config) {
     //var tsProject = ts.createProject("tsconfig.json");
     //var watch = require( 'gulp-watch' );
 
-    gulp.task('clean', [], function() {
+    gulp.task('little-clean', [], function() {
         console.log('Clean all files in lib/, bin/, and site/ folders');
-        return gulp.src( ['bin/*', 'lib/*', 'maps/*', 'site/*'], { read: false }).pipe(clean());
+        return gulp.src( ['bin', 'lib', 'maps', 'site'], { read: false }).pipe(clean());
     });
 
-
-    gulp.task('compilehtml', [ 'compilenunjucks' ], function() {});
+    gulp.task('little-compilehtml', [ 'little-compilenunjucks' ], function() {});
 
     // add revision-hash to js and css file names
     var tsConfig = {
@@ -67,7 +66,7 @@ module.exports.defineTasks = function(config) {
         // declaration: true
     };
 
-    gulp.task( 'compilets', [], function() {
+    gulp.task( 'little-compilets', [], function() {
         const tsResult = gulp.src( ['src/**/*.ts'], 
                 { base: basePath })
             .pipe( sourcemaps.init() )
@@ -84,7 +83,7 @@ module.exports.defineTasks = function(config) {
     // see https://zellwk.com/blog/nunjucks-with-gulp/
     // Also incorporating markdown support with nunjucks-markdown.
     //
-    gulp.task( 'compilenunjucks', [], function() {
+    gulp.task( 'little-compilenunjucks', [], function() {
         gulp.src( 
             [ basePath + '/**/*.html' ],
             { base: basePath }
@@ -93,61 +92,34 @@ module.exports.defineTasks = function(config) {
             nunjucksRender(
                 {
                     manageEnv:nunjucksManageEnv, 
-                    envOptions:{autoescape:false}, 
-                    path: [ basePath + '/lib' ]
+                    envOptions:{ autoescape: false }, 
+                    path: [ basePath ]
                 }
             ) ) // path: [ "src/templates" ], 
         .on('error', console.log)
         .pipe(gulp.dest('.'));
     });
 
-    gulp.task('compile', [ 'compilehtml', 'compilets' ], function() {
+    gulp.task('little-compile', [ 'little-compilehtml', 'little-compilets' ], function() {
     // place code for your default task here
     //console.log( "Hello, World!" );
     //gulp.src( "src/**/*" ).pipe( gulp.dest( "lib/" ) );
     });
 
-    gulp.task('default', [ 'compile' ], function() {
-    // place code for your default task here
-    //console.log( "Hello, World!" );
-    //gulp.src( "src/**/*" ).pipe( gulp.dest( "lib/" ) );
-    });
-
-    gulp.task('watchts', function () {
+    gulp.task('little-watchts', function () {
         // Endless stream mode 
-        return gulp.watch('src/**/*.ts', [ 'compilets' ] );
+        return gulp.watch('src/**/*.ts', [ 'little-compilets' ] );
     });
 
-    gulp.task( 'watchhtml', function () {
-        return gulp.watch( ['src/**/*.html', 'src/**/*.css'], [ 'compilehtml' ] );     
+    gulp.task( 'little-watchhtml', function () {
+        return gulp.watch( ['src/**/*.html', 'src/**/*.css'], [ 'little-compilehtml' ] );     
     });
 
 
-    gulp.task( 'watch', [ 'watchts', 'watchhtml' ], function() {
+    gulp.task( 'little-watch', [ 'little-watchts', 'little-watchhtml' ], function() {
     });
 
-    gulp.task( 'compileclean', function(cb) {
-        return gulpSequence( 'clean', 'compile' )(cb);
-    });
-
-    gulp.task( 'deploy', [ 'compileclean' ], function(cb) {
-        const pwdPath = process.cwd();
-        const imageName = "frickjack/s3cp:1.0.0";
-        const commandStr = "yes | docker run --rm --name s3gulp -v littleware:/root/.littleware -v '" +
-            pwdPath + ":/mnt/workspace' " + imageName + " -copy /mnt/workspace/lib/ s3://apps.frickjack.com/";
-
-        console.log( "Running: " + commandStr );
-
-        exec( commandStr, 
-            function (err, stdout, stderr) {
-                console.log(stdout);
-                console.log(stderr);
-                if ( err ) {
-                    //reject( err );
-                } else {
-                    cb();
-                }
-            }
-        );
+    gulp.task( 'little-compileclean', function(cb) {
+        return gulpSequence( 'little-clean', 'little-compile' )(cb);
     });
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
+const gulp = require('gulp');
 const gulpHelper = require('./gulpHelper');
 const basePath = "src/@littleware/little-elements";
 
 
-gulpHelper.exportTasks( { basePath } );
+gulpHelper.defineTasks( { basePath } );
+
+gulp.task('default', [ 'little-compile' ], function() {
+    // place code for your default task here
+    //console.log( "Hello, World!" );
+    //gulp.src( "src/**/*" ).pipe( gulp.dest( "lib/" ) );
+    });
