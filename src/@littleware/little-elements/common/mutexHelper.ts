9dddf318f5eb35de1b93424e7fb26ca228e57ee6
Reuben Pasquini <reuben@frickjack.com>
2019-12-30 19:37:05 -0600

    chore(mutexHelper): add backoff and LazyThing.then

--- src/@littleware/little-elements/common/mutexHelper.ts
+++ src/@littleware/little-elements/common/mutexHelper.ts
@@ -12,7 +12,8 @@ export class LazyThing<T> {
 
     /**
      * @param loader lambda that loads the thing on demand
-     * @param ttlSecs number of seconds to cache the thing before triggering a reload
+     * @param ttlSecs number of seconds to cache the thing before triggering a reload,
+     *          default to never reload
      */
     constructor(loader: () => Promise<T>, ttlSecs = -1) {
         this.loader = loader;
@@ -22,10 +23,10 @@ export class LazyThing<T> {
     /**
      * Refresh if ttl expired or force true
      * @param force
-     * @return cached this.thing if force is false, else the promise
-     *                that loads the new data when reload is done
+     * @return cached this.thing and the promise
+     *    that loads the new data when reload is done
      */
-    public refreshIfNecessary(force: boolean = false): Promise<T> {
+    public refreshIfNecessary(force: boolean = false): { current: Promise<T>, next: Promise<T> } {
         if (this._thing) {
             // trigger reload in background if necessary
             if (
@@ -47,25 +48,23 @@ export class LazyThing<T> {
                     (err) => {
                         this._lastLoadTime = Date.now();
                         this.reload = null;
+                        // leave the previously loaded thing in place
                     },
                 );
             }
-            if (force) {
-                return this.reload;
-            } else {
-                return this._thing;
-            }
+            return { current: this._thing, next: this.reload || this._thing };
         }
+
         // initial load ...
         this._thing = this.loader();
         this._thing.finally(() => {
             this._lastLoadTime = Date.now();
         });
-        return this._thing;
+        return { current: this._thing, next: this._thing };
     }
 
     get thing(): Promise<T> {
-        return this.refreshIfNecessary(false);
+        return this.refreshIfNecessary(false).current;
     }
 
     /**
@@ -76,15 +75,37 @@ export class LazyThing<T> {
     get lastLoadTime() {
         return this._lastLoadTime;
     }
+
+    /**
+     * Apply a transformation on this thing.
+     * Shortcut for new LazyThing(() => parent.thing.then(...))
+     * Note that refresh() on the child thing
+     * does not force a refresh on the parent, and
+     * vice versa, but the child inherits the ttl from
+     * its parent.
+     * 
+     * @param lambda 
+     */
+
+    public then<R>(lambda:(x:T) => R):LazyThing<R>;
+    public then<R>(lambda:(x:T) => Promise<R>):LazyThing<R> {
+        const result = new LazyThing(() => {
+            return this.refreshIfNecessary(false).next.then((thing) => lambda(thing));
+        }, this.ttlSecs);
+        return result;
+    }
 }
 
 export function sleep(ms: number): Promise<void> {
     return new Promise(
-        (resolve) => {
-            setTimeout(() => resolve(), ms);
-        },
-    );
-
+            (resolve) => {
+                if (ms) {
+                    setTimeout(() => resolve(), ms);
+                } else {
+                    resolve();
+                }
+            },
+        );
 }
 
 /**
@@ -105,4 +126,98 @@ export function squish(lambda: () => Promise<any>): () => Promise<any> {
         }
         return inFlight;
     };
+}
+
+/**
+ * Return an iterator with the number of ms to
+ * backoff before each retry
+ * 
+ * @param maxRetries default 3, silently clamped to minimum 1, max 10
+ * @param backoffMs default 200, silently clamped to minimum 100ms, max 10000ms
+ * @return iterable iterator with backoff values starting from zero
+ */
+export function backoffIterator(maxRetriesIn, backoffMsIn) {
+    const maxRetries = (() => {
+        let valid = maxRetriesIn;
+        if (valid < 1) {
+            valid = 1;
+        }
+        if (valid > 10) {
+            valid = 10;
+        }
+        return valid;
+    })();
+    const backoffMs = (() => {
+        let valid = backoffMsIn;
+        if (valid < 100) {
+            valid = 100;
+        }
+        if (valid > 10000) {
+            valid = 10000;
+        }
+        return valid;
+    })();
+    const step = (() => {
+        let jitter = Math.floor(Math.random() * backoffMs/10);
+        if (Math.random() < 0.5) {
+            jitter = 0 - jitter;
+        }
+        return backoffMs + jitter;
+    })();
+
+    let count = 0;
+    let lastResult = 0;
+
+    return {
+        next: function() {
+            const result = {
+                value: lastResult + lastResult,
+                done: count > maxRetries,
+            };
+            if (!result.done) {
+                lastResult = result.value || step/2;
+                count += 1;
+            }
+            return result;
+        },
+        [Symbol.iterator]: function() { return this; }
+    };
+}
+
+/**
+ * Return a lambda that retries up to
+ * maxRetries times with jitter exponential backoff
+ * of backoffMs, 2*backoffMs, ... 2^n*backoffMs
+ * capped at a 120000 ms backoff with a jitter of backoffMs/10
+ * 
+ * @param lambda 
+ * @param maxRetries default 3, silently clamped to minimum 1, max 10
+ * @param backoffMs default 200, silently clamped to minimum 100ms, max 10000ms
+ */
+export function backoff<T>(lambda: () => Promise<T>, maxRetries=10, backoffMs=200): () => Promise<T> {
+    const it = backoffIterator(maxRetries, backoffMs);
+    const helper:() => Promise<T> = function() {
+        const next = it.next();
+        if (next.done) {
+            return sleep(next.value).then(() => lambda());
+        } else {
+            return sleep(next.value).then(() => lambda()).catch(
+                (err) => {
+                    return helper();
+                }
+            );
+        }
+    };
+    return helper;
+}
+
+
+/**
+ * An asynchronous client could overload a backend service with requests.
+ * A throttle limits the number of concurrently running requests by
+ * queueing them up, and circuit breaks (fast fails) requests once some
+ * queue length threshold is exceeded.
+ */
+export function throttle<T>(lambda: () => Promise<T>, maxConcurrency=4, maxQueueLen=20): () => Promise<T> {
+    throw new Error('not yet implemented');
 }
--- src/@littleware/little-elements/common/spec/mutexHelperSpec.ts
+++ src/@littleware/little-elements/common/spec/mutexHelperSpec.ts
@@ -1,4 +1,4 @@
-import { LazyThing, sleep, squish } from "../mutexHelper.js";
+import { backoff, backoffIterator, LazyThing, sleep, squish } from "../mutexHelper.js";
 
 describe( "the littleware.mutexHelper", () => {
     it("can sleep for a few seconds", (done) => {
@@ -41,12 +41,118 @@ describe( "the littleware.mutexHelper", () => {
         const thing4 = await lazy.thing;
         // console.log(`test thing 4: ${thing4 === v[0]}`);
         expect(thing4).not.toBe(v[0]);
-        const thing5 = await lazy.refreshIfNecessary(false);
+        const thing5 = await lazy.refreshIfNecessary(false).next;
         expect(thing5).toBe(thing4);
-        const thing6 = await lazy.refreshIfNecessary(true);
+        const thing6 = await lazy.refreshIfNecessary(true).next;
         // console.log(`test thing 5: ${thing5 === thing6}`);
         expect(thing6).not.toBe(thing5);
         expect(lazy.lastLoadTime).toBeGreaterThan(startMs);
         done();
+    });
+
+    it("can transform a thing", async (done) => {
+        let counter = 0;
+        const lazy = new LazyThing(() => sleep(100).then(() => { counter += 1; return counter; }), 1
+        ).then(num => `The number is ${num}`);
+        
+        // first value should be 1
+        const str1 = await lazy.thing;
+        expect(str1).toBe(`The number is 1`);
+        // second value will still be 1, but it triggered an update
+        const str2 = await sleep(1500).then(() => lazy.thing);
+        expect(str2).toBe(str1);
+        // third value should pickup the update
+        const str3 = await sleep(200).then(() => lazy.thing);
+        expect(str3).toBe(`The number is 2`);
+        done();
+    });
+
+    it("can setup a backoff iterator", () => {
+        const numRetries = 3;
+        const backoffMs = 100;
+        const it = backoffIterator(numRetries, backoffMs);
+        let count = 0;
+        let lastValue = 0;
+        for (let value of it) {
+            if (count === 1) { // first retry
+                expect(value).toBeGreaterThan(backoffMs / 2);
+                expect(value).toBeLessThan(backoffMs * 3/2)
+            } else {
+                expect(value).toBe(lastValue + lastValue);
+            }
+            lastValue = value;
+            expect(count).toBeLessThan(numRetries + 1);
+            count += 1;
+        }
+        expect(count).toBe(numRetries + 1);
+        for (let i=0; i < 2; i++) {
+            const last = it.next();
+            expect(last.done).toBe(true);
+            expect(last.value).toBe(lastValue + lastValue);
+        }
+    });
+
+    it("can backoff and retry to failure", (done) => {
+        const maxRetries = 3;
+        const backoffMs = 100;
+        const startMs = Date.now();
+        let lastRunMs = 0;
+        let count = 0;
+        
+        const lambda = () => {
+            const nowMs = Date.now();
+            if (lastRunMs) {
+                expect(nowMs - lastRunMs).toBeGreaterThan(count * backoffMs / 2);
+            }
+            expect(count).toBeLessThan(maxRetries+2);
+            count += 1;
+            lastRunMs = nowMs;
+            return Promise.reject('always fail');
+        }
+
+        backoff(lambda, maxRetries, backoffMs)().then(
+            () => {
+                done.fail('lambda should always fail');
+            }
+        ).catch(
+            () => {
+                expect(count).toBe(maxRetries + 2);
+                expect(Date.now() - startMs).toBeGreaterThan(backoffMs);
+                done();
+            }
+        );
+    });
+
+    it("can backoff and retry to success", (done) => {
+        const maxRetries = 3;
+        const backoffMs = 100;
+        const startMs = Date.now();
+        let lastRunMs = 0;
+        let count = 0;
+        
+        const lambda = () => {
+            const nowMs = Date.now();
+            if (lastRunMs) {
+                expect(nowMs - lastRunMs).toBeGreaterThan(count * backoffMs / 2);
+            }
+            expect(count).toBeLessThan(maxRetries+2);
+            count += 1;
+            lastRunMs = nowMs;
+            if (count < 2) {
+                return Promise.reject(`fail on ${count}`);
+            }
+            return Promise.resolve(`success on ${count}`);
+        }
+
+        backoff(lambda, maxRetries, backoffMs)().then(
+            (str) => {
+                expect(str).toBe("success on 2");
+                done();
+            }
+        ).catch(
+            () => {
+                done.fail('backoff should succeed on 2nd retry');
+            }
+        );
     });
 });
